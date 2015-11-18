0461509d6891bb605a5b589216fc22a53f0a67cf
Pablo Pastore <pabloapast@gmail.com>
2015-11-18 00:04:12 -0300

    finished week02

--- week02/funsets/src/main/scala/funsets/FunSets.scala
+++ week02/funsets/src/main/scala/funsets/FunSets.scala
@@ -51,25 +51,25 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (!filter(s, p)(a)) false
+      else if (a == 1000) true
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-1000)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (x: Int) => !p(x))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = (x: Int) => s(f(x))
   
   /**
    * Displays the contents of a set
